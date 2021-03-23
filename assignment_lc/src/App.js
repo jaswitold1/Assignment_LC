7a71fb2a33fc663b865218505cc444677654b466
Witold Jas <jaswitold1@gmail.com>
2021-03-23 22:57:34 +0100

    fetching data from API added

--- assignment_lc/src/App.js
+++ assignment_lc/src/App.js
@@ -2,6 +2,18 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "GET",
+    })
+      .then((resp) => resp.json())
+      .then((resp) => setData(resp))
+      .catch((error) => console.log(error));
+    return () => {
+      setData([]);
+    };
+  }, []);
   return (
     <div className='App'>
       <header>
